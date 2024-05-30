167622dd66919a41bdea909508c3dfca2210f5b0
Gunneng <s.gunneng@gmail.com>
2024-05-30 11:47:10 +0200

    feat: add function

---
+++ integers/adder.go
@@ -0,0 +1,6 @@
+package integers
+
+// Add takes two integers and returns the sum
+func Add(x, y int) (sum int) {
+	return x + y
+}
---
+++ integers/adder_test.go
@@ -0,0 +1,12 @@
+package integers
+
+import "testing"
+
+func TestAdder(t *testing.T) {
+	got := Add(2, 2)
+	want := 4
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
