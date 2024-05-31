264d92160f44ae7a4f2b8a3170ccd903f8321638
Gunneng <s.gunneng@gmail.com>
2024-05-31 11:03:27 +0200

    feat: sum tails

--- sum/slice.go
+++ sum/slice.go
@@ -8,11 +8,24 @@ func Sum(n []int) int {
 	return sum
 }
 
-// O(n^2)
 func SumAll(numbers ...[]int) []int {
 	var results []int
 	for _, s := range numbers {
 		results = append(results, Sum(s))
 	}
+	return results
+}
+
+func SumAllTails(numbers ...[]int) []int {
+	results := []int{}
+
+	for _, slic := range numbers {
+		if len(slic) <= 0 {
+			results = append(results, 0)
+		} else {
+			results = append(results, Sum(slic[1:]))
+		}
+	}
+
 	return results
 }
--- sum/slice_test.go
+++ sum/slice_test.go
@@ -25,6 +25,26 @@ func TestSumAll(t *testing.T) {
 	}
 }
 
+func TestSumAllTails(t *testing.T) {
+	t.Run("sum of slices", func(t *testing.T) {
+		want := []int{8, 10}
+		got := SumAllTails([]int{4, 8}, []int{0, 10})
+
+		if !slices.Equal(want, got) {
+			t.Errorf("want %d, got %d", want, got)
+		}
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		want := []int{0, 3}
+		got := SumAllTails([]int{}, []int{0, 1, 2})
+
+		if !slices.Equal(want, got) {
+			t.Errorf("want %d, got %d", want, got)
+		}
+	})
+}
+
 func assert(t testing.TB, got, want int, numbers []int) {
 	t.Helper()
 	if got != want {
