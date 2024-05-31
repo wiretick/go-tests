0cad75b3921ff53bc25b4af11c250fe471f5fa65
Gunneng <s.gunneng@gmail.com>
2024-05-31 12:32:10 +0200

    feat: methods

--- structure/shapes_test.go
+++ structure/shapes_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestPerimeter(t *testing.T) {
 	rect := Rectangle{Width: 5., Height: 5.}
 	want := 20.
-	got := Perimeter(rect)
+	got := rect.Perimeter()
 
 	if want != got {
 		t.Errorf("want %.2f, got %.2f", want, got)
@@ -13,11 +13,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rect := Rectangle{Width: 5., Height: 5.}
-	want := 25.
-	got := Area(rect)
+	t.Run("for rectangle", func(t *testing.T) {
+		rect := Rectangle{Width: 5., Height: 5.}
+		want := 25.
+		got := rect.Area()
 
-	if want != got {
-		t.Errorf("want %.2f, got %.2f", want, got)
-	}
+		if want != got {
+			t.Errorf("want %.2f, got %.2f", want, got)
+		}
+	})
+
+	t.Run("for rectangle", func(t *testing.T) {
+		circle := Circle{Radius: 10.}
+		want := 314.1592653589793
+		got := circle.Area()
+
+		if want != got {
+			t.Errorf("want %g, got %g", want, got)
+		}
+	})
 }
