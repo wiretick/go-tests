95f35ee01289aea9bd8fe4c6e68fb6fe2ca7f45c
Gunneng <s.gunneng@gmail.com>
2024-05-31 12:39:13 +0200

    feat: interfaces

--- structure/shapes_test.go
+++ structure/shapes_test.go
@@ -13,23 +13,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("for rectangle", func(t *testing.T) {
-		rect := Rectangle{Width: 5., Height: 5.}
-		want := 25.
-		got := rect.Area()
-
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if want != got {
 			t.Errorf("want %.2f, got %.2f", want, got)
 		}
+	}
+
+	t.Run("for rectangle", func(t *testing.T) {
+		rect := Rectangle{Width: 5., Height: 5.}
+		checkArea(t, rect, 25.)
 	})
 
 	t.Run("for rectangle", func(t *testing.T) {
 		circle := Circle{Radius: 10.}
-		want := 314.1592653589793
-		got := circle.Area()
-
-		if want != got {
-			t.Errorf("want %g, got %g", want, got)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
