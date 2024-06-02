c4e8fb6a0ed219064bce2350a356f72f5602dd50
Gunneng <s.gunneng@gmail.com>
2024-06-02 11:32:01 +0200

    ref: use errors for unknown values

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,17 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(term string) string {
-	return d[term]
+var ErrUnknownTerm = errors.New("Unknown search term")
+
+func (d Dictionary) Search(term string) (string, error) {
+	result := d[term]
+
+	if result == "" {
+		return result, ErrUnknownTerm
+	}
+
+	return result, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,12 +1,31 @@
 package maps
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
-	dict := Dictionary{"hello": "Hello is a casual greeting"}
-	got := dict.Search("hello")
-	want := "Hello is a casual greeting"
+	t.Run("define term", func(t *testing.T) {
+		dict := Dictionary{"hello": "Hello is a casual greeting"}
+		got, _ := dict.Search("hello")
+		want := "Hello is a casual greeting"
 
+		assertString(t, got, want)
+	})
+
+	t.Run("search for unknown term", func(t *testing.T) {
+		dict := Dictionary{"hello": "Hello is a casual greeting"}
+		_, got := dict.Search("unknown")
+		want := ErrUnknownTerm
+
+		if got != want {
+			t.Errorf("expected unknown search term error")
+		}
+	})
+}
+
+func assertString(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("want %q got %q", want, got)
 	}
