a8406e47da9f35d785e3dc155ecef4a5d831078f
Gunneng <s.gunneng@gmail.com>
2024-06-03 15:44:44 +0200

    feat: slow unmocked test

--- mockme/countdown.go
+++ mockme/countdown.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
+)
+
+const (
+	ending    = "GO!"
+	countdown = 3
 )
 
 func Countdown(w io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdown; i > 0; i-- {
 		fmt.Fprintf(w, "%d ", i)
+		time.Sleep(1 * time.Second)
 	}
-	fmt.Fprintf(w, "GO!")
+	fmt.Fprintf(w, ending)
 }
 
 func main() {
