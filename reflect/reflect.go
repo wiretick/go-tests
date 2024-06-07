0ef375df8611c29f8e041fc8efca76e81d8cf37d
Gunneng <s.gunneng@gmail.com>
2024-06-07 17:27:19 +0200

    use one shared loop

--- reflect/reflect.go
+++ reflect/reflect.go
@@ -9,16 +9,21 @@ func Walk(x interface{}, fn func(string)) {
 		val = val.Elem()
 	}
 
+	count := 0
+	var getField func(int) reflect.Value
+
 	switch val.Kind() {
-	case reflect.Struct:
-		for i := range val.NumField() {
-			Walk(val.Field(i).Interface(), fn)
-		}
-	case reflect.Slice:
-		for i := range val.Len() {
-			Walk(val.Index(i).Interface(), fn)
-		}
 	case reflect.String:
 		fn(val.String())
+	case reflect.Struct:
+		getField = val.Field
+		count = val.NumField()
+	case reflect.Slice:
+		getField = val.Index
+		count = val.Len()
+	}
+
+	for i := range count {
+		Walk(getField(i).Interface(), fn)
 	}
 }
