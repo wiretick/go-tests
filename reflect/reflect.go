44f4cc89cbd11e5c13b77ced64497c3b9cfe416b
Gunneng <s.gunneng@gmail.com>
2024-06-07 13:47:42 +0200

    feat: basic reflection

---
+++ reflect/reflect.go
@@ -0,0 +1,12 @@
+package reflect
+
+import "reflect"
+
+func Walk(x interface{}, fn func(string)) {
+	val := reflect.ValueOf(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fn(field.String())
+	}
+}
---
+++ reflect/reflect_test.go
@@ -0,0 +1,43 @@
+package reflect
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with only one string field",
+			struct {
+				Name string
+			}{"Tom"},
+			[]string{"Tom"},
+		},
+		{
+			"struct with two string fields",
+			struct {
+				Name string
+				Town string
+			}{"Tom", "Oslo"},
+			[]string{"Tom", "Oslo"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			Walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("want %v, got %v", test.ExpectedCalls, got)
+			}
+		})
+	}
+}
