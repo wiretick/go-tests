c68d70ec00f8d3abf7bec84d13c4c8213334a84b
Gunneng <s.gunneng@gmail.com>
2024-06-07 14:40:05 +0200

    ref: if to switch

--- reflect/reflect.go
+++ reflect/reflect.go
@@ -8,11 +8,10 @@ func Walk(x interface{}, fn func(string)) {
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
 
-		if field.Kind() == reflect.String {
+		switch field.Kind() {
+		case reflect.String:
 			fn(field.String())
-		}
-
-		if field.Kind() == reflect.Struct {
+		case reflect.Struct:
 			Walk(field.Interface(), fn)
 		}
 	}
